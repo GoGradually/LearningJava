f2c39daf41b4ab19cb4e9de82e03ceb8ae4703ca
GoGradually <chkun3109@gmail.com>
2024-04-01 22:09:25 +0900

    Spring-DB1 2 start

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,59 @@
+package hello.jdbc.connection;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        // 드라이버 매니저를 이용해서 커넥션 가져오기
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+
+    }
+
+    @Test
+    void dataSourceDriverMananger() throws SQLException {
+        // 드라이버매니저 데이터소스 객체를 생성한 뒤
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        // 해당 객체의 getConnection 으로 커넥션을 가져온다.
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        // 매개변수를 줄 필요가 없다! -> 설정과 사용의 분리!
+        // 데이터소스를 사용하는 방식은 처음 객체를 생성할 때만 필요한 파라미터를 넘겨두고, 커넥션을 획득할 때는 단순히 getConnection() 만 호출하면 된다.
+        // URL, USERNAME, PASSWORD 에 의존하지 않아도 된다.
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
