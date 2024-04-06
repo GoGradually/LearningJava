f6bfe88b0c2a00da56d327bb0de0e82ac2d7d3bd
GoGradually <chkun3109@gmail.com>
2024-04-07 06:45:54 +0900

    Spring DB2 2 start

---
+++ itemservice-db/src/main/java/hello/itemservicedb/ItemserviceDbApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservicedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemserviceDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemserviceDbApplication.class, args);
+	}
+
+}
