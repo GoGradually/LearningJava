9d8b7cb1a5980ee2bc9a54a03be2095c66095c8c
GoGradually <chkun3109@gmail.com>
2024-04-10 23:12:48 +0900

    Spring DB2 5

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,16 +1,24 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+//@Table(name = "item")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // ddl 생성 시 활용하는 정보
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    //필수
     public Item() {
     }
 
