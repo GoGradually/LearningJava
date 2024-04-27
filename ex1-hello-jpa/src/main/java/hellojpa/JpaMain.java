384173209066c0d28578782c66995424508bbd40
GoGradually <chkun3109@gmail.com>
2024-04-27 23:44:54 +0900

    JPA basic 3 start

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -10,7 +12,29 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+//            Member findMember = em.find(Member.class, 1L);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(2)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+
+        }
         emf.close();
     }
 }
