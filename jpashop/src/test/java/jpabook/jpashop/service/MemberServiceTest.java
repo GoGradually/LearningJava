bcf7574a04265e87c0234e1d647e9429e327ade0
GoGradually <chkun3109@gmail.com>
2024-07-21 22:45:06 +0900

    아이템 도메인 개발

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void 회원가입() {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        em.flush();
+
+        assertEquals(member, memberService.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+}
