abd98de9a8dd146b6c68bdf118fce3c4b8ffc2b6
GoGradually <chkun3109@gmail.com>
2024-08-09 02:28:10 +0900

    확장 기능

--- data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -8,9 +9,15 @@ public class MemberDto {
     private String username;
     private String teamName;
 
+
     public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
