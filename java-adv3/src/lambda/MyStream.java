54b0f80ff5d5b94659da4c160ea4a0c12ecbdc6b
GoGradually <chkun3109@gmail.com>
2025-05-13 18:16:19 +0900

    스트림의 지연 연산 확인

---
+++ java-adv3/src/lambda/MyStream.java
@@ -0,0 +1,84 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class MyStream<T> {
+    private final List<T> list;
+
+    private MyStream(List<T> list) {
+        this.list = list;
+    }
+
+    public static <T> MyStream<T> of(List<T> list) {
+        return new MyStream<>(list);
+    }
+
+    public MyStream<T> filter(Predicate<T> predicate) {
+        List<T> newList = new ArrayList<>();
+        for (T t : list) {
+            if (predicate.test(t)) {
+                newList.add(t);
+            }
+        }
+        return new MyStream<>(newList);
+    }
+
+    public <R> MyStream<R> map(Function<T, R> function) {
+        List<R> newList = new ArrayList<>();
+        for (T t : list) {
+            newList.add(function.apply(t));
+        }
+        return new MyStream<>(newList);
+    }
+
+    public void forEach(Consumer<T> consumer) {
+        for (T t : list) {
+            consumer.accept(t);
+        }
+    }
+
+    public List<T> toList() {
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        System.out.println("================My Stream 시작================");
+        MyStream.of(integers)
+                .filter(n -> {
+                    boolean isOdd = n % 2 == 1;
+                    System.out.println("값 " + n + " 홀수 여부 확인: " + isOdd);
+                    return isOdd;
+                })
+                .map(n -> {
+                    int result = n * 2;
+                    System.out.println("변환된 값 = " + n + "->" + result);
+                    return result;
+                });
+
+        System.out.println("================My Stream 종료================");
+
+        System.out.println("================Java Stream 시작================");
+        integers.stream()
+                .filter(n -> {
+                    boolean isOdd = n % 2 == 1;
+                    System.out.println("값 " + n + " 홀수 여부 확인: " + isOdd);
+                    return isOdd;
+                })
+                .map(n -> {
+                    int result = n * 2;
+                    System.out.println("변환된 값 = " + n + "->" + result);
+                    return result;
+                });
+        System.out.println("================Java Stream 종료================");
+    }
+}
+
+
+
+
